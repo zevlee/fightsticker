c63091f2a41ee4dd0f5dba589a43df17a3be72e0
Zev Lee <60147316+zevlee@users.noreply.github.com>
2025-01-26 20:23:38 -0600

    chore: Increment version to v0.5.1

--- fightsticker/__init__.py
+++ fightsticker/__init__.py
@@ -4,7 +4,7 @@ from os.path import dirname, join
 from platformdirs import user_config_dir
 
 # Version
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 # Application name
 APPNAME = "Fightsticker"
 # Application ID
