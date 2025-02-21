b5813f094623bcf11204366e8b5d1b509262aa5b
Zev Lee <60147316+zevlee@users.noreply.github.com>
2025-02-21 04:26:06 -0600

    fix: Change the error handling for images
    The error handling was incorrect. This commit fixes the issue.

--- fightsticker/fightstick.py
+++ fightsticker/fightstick.py
@@ -86,7 +86,7 @@ class LayoutScene(_BaseScene):
         try:
             image = pyglet.resource.image(self.images[name])
         except KeyError:
-            image = "none.png"
+            image = pyglet.resource.image("none.png")
         try:
             position = self.layout[name]
         except KeyError:
